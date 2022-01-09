9ec7a447910464f3b84bc5de2f80c2e2d84aff1b
saharmi22 <saharmilg15@gmail.com>
2022-01-09 16:03:13 +0200

    fixed cmake

---
+++ cmake-build-debug/CMakeFiles/Wet2_DS.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/tmp/tmp.HNLZ2ldeVz/Group.cpp" "/tmp/tmp.HNLZ2ldeVz/cmake-build-debug/CMakeFiles/Wet2_DS.dir/Group.cpp.o"
+  "/tmp/tmp.HNLZ2ldeVz/HashTable.cpp" "/tmp/tmp.HNLZ2ldeVz/cmake-build-debug/CMakeFiles/Wet2_DS.dir/HashTable.cpp.o"
+  "/tmp/tmp.HNLZ2ldeVz/HashTableNode.cpp" "/tmp/tmp.HNLZ2ldeVz/cmake-build-debug/CMakeFiles/Wet2_DS.dir/HashTableNode.cpp.o"
+  "/tmp/tmp.HNLZ2ldeVz/Player.cpp" "/tmp/tmp.HNLZ2ldeVz/cmake-build-debug/CMakeFiles/Wet2_DS.dir/Player.cpp.o"
+  "/tmp/tmp.HNLZ2ldeVz/UnionFind.cpp" "/tmp/tmp.HNLZ2ldeVz/cmake-build-debug/CMakeFiles/Wet2_DS.dir/UnionFind.cpp.o"
+  "/tmp/tmp.HNLZ2ldeVz/UnionNode.cpp" "/tmp/tmp.HNLZ2ldeVz/cmake-build-debug/CMakeFiles/Wet2_DS.dir/UnionNode.cpp.o"
+  "/tmp/tmp.HNLZ2ldeVz/main.cpp" "/tmp/tmp.HNLZ2ldeVz/cmake-build-debug/CMakeFiles/Wet2_DS.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ cmake-build-debug/CMakeFiles/Wet2_DS.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Wet2_DS.dir/main.cpp.o"
+  "CMakeFiles/Wet2_DS.dir/HashTable.cpp.o"
+  "CMakeFiles/Wet2_DS.dir/HashTableNode.cpp.o"
+  "CMakeFiles/Wet2_DS.dir/UnionFind.cpp.o"
+  "CMakeFiles/Wet2_DS.dir/UnionNode.cpp.o"
+  "CMakeFiles/Wet2_DS.dir/Player.cpp.o"
+  "CMakeFiles/Wet2_DS.dir/Group.cpp.o"
+  "Wet2_DS.pdb"
+  "Wet2_DS"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Wet2_DS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)

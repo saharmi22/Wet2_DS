ec3790d43a05c02a4df53254674b171b81475668
saharmi22 <saharmilg15@gmail.com>
2022-01-10 12:10:07 +0200

    added PlayersManager.cpp and PlayersManager.h

---
+++ PlayersManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sahar on 10/01/2022.
+//
+
+#include "PlayersManager.h"
---
+++ PlayersManager.h
@@ -0,0 +1,14 @@
+//
+// Created by sahar on 10/01/2022.
+//
+
+#ifndef WET2_DS_PLAYERSMANAGER_H
+#define WET2_DS_PLAYERSMANAGER_H
+
+
+class PlayersManager {
+
+};
+
+
+#endif //WET2_DS_PLAYERSMANAGER_H
